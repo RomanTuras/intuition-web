ff87cd07df8623b54d7c3315fba395db201f2eb8
salden <salden.com@gmail.com>
2021-10-23 16:38:01 +0300

    Add routing

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Home component</h1>
+    <router-link :to="{
+      name: 'about'
+    }">
+      <h3>About</h3>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
