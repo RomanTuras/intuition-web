d5df88b10b912d644c57d1a9fa3ca27367fe8f1f
salden <salden.com@gmail.com>
2021-10-23 23:03:40 +0300

    Add scss loader for styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,39 @@
 <template>
   <div>
+    <button  @click="toggleTheme" aria-label="Toggle themes">
+      <span v-if="this.theme === 'darkMode'"> Light</span>
+      <span v-else> Dark</span>
+    </button>
     <h1>Home component</h1>
     <router-link :to="{
       name: 'about'
     }">
       <h3>About</h3>
     </router-link>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  data: () => ({
+    theme: ''
+  }),
+  mounted() {
+    let localTheme = localStorage.getItem('theme'); //gets stored theme value if any
+    document.documentElement.setAttribute('data-theme', localTheme); // updates the data-theme attribute
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme === 'darkMode' ? '' : 'darkMode';
+      document.documentElement.setAttribute('data-theme', this.theme);
+      localStorage.setItem('theme', this.theme);
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@import "src/scss/app";
 </style>
