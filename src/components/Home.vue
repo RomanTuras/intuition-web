85af7790a0844db879afd4ad9e857c4ec1690a53
salden <salden.com@gmail.com>
2021-10-24 18:42:47 +0300

    Add i18n localization

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,12 +4,25 @@
       <span v-if="this.theme === 'darkMode'">Light</span>
       <span v-else> Dark</span>
     </button>
-    <h1 class="text-center">Home component</h1>
+    <h1 class="text-center">{{ $t('appTitle')}}</h1>
     <router-link :to="{
       name: 'about'
     }">
       <h3>About</h3>
     </router-link>
+
+    <div>
+      <ul>
+        <li v-for="locale in locales" :key="locale" @click="switchLocale(locale)">
+          {{locale}}
+        </li>
+      </ul>
+    </div>
+
+    <div class="mt-4">
+      Get current locale
+      {{this.$i18n.locale}}
+    </div>
 
   </div>
 </template>
@@ -18,7 +31,8 @@
 export default {
   name: "home",
   data: () => ({
-    theme: ''
+    theme: '',
+    locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(',')
   }),
   mounted() {
     let localTheme = localStorage.getItem('theme'); //gets stored theme value if any
@@ -29,6 +43,11 @@ export default {
       this.theme = this.theme === 'darkMode' ? '' : 'darkMode';
       document.documentElement.setAttribute('data-theme', this.theme);
       localStorage.setItem('theme', this.theme);
+    },
+    switchLocale(locale) {
+      if (this.$i18n.locale !== locale) {
+        this.$i18n.locale = locale;
+      }
     }
   }
 }
