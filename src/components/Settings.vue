2fa2c528f051d7e2a24fd48e1eeebd01d0ee4677
salden <salden.com@gmail.com>
2021-11-17 13:47:38 +0200

    Feature. Selecting skin modal

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -3,9 +3,11 @@
     <nav-bar @onBack="onBack"  :title="$t('settingsTitle')" :is-home=false />
 
     <div class="column ml-auto mr-auto mt-4 mb-4 flex-wrap md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+
+<!--      Common Section-->
       <div class="card w-100 mx-2 sm:mx-4 md:m-0 shadow-md rounded-xl" >
         <div class="card-title">
-          <h2 class="about-title">{{ $t('settingsCommon') }}</h2>
+          <h2 class="about-title cursor-default">{{ $t('settingsCommon') }}</h2>
 
 <!--          Locale-->
           <div class="my-5 flex justify-between cursor-pointer" @click="selectLocal=true">
@@ -49,9 +51,60 @@
 
 
       </div>
+<!--      End Common Section-->
+
+<!--      Application Section-->
+      <div class="card w-100 mx-2 sm:mx-4 md:mx-0 mt-4 shadow-md rounded-xl" >
+        <div class="card-title">
+          <h2 class="about-title cursor-default">{{ $t('settingsApplication') }}</h2>
+
+          <!--          Exercise Skin -->
+          <div class="my-5 flex justify-between cursor-pointer" @click="selectSkin=true">
+            <div class="flex flex-row items-start">
+              <div class="mr-4 mt-1">
+                <skin-icon style="width: 26px" />
+              </div>
+              <div class="">
+                <p class="settings-title block">{{ $t('settingsExercisesSkins') }}</p>
+                <span class="settings-subtitle block">{{$t(skin)}}</span>
+              </div>
+            </div>
+            <div class="float-right align-baseline">
+              <svg id="down-arrow" class="fill-current mx-auto my-4 mr-5" width="12" height="6" viewBox="0 0 12 6" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 6L11.1962 0H0.803848L6 6Z" />
+              </svg>
+            </div>
+
+          </div>
+          <!--          Exercise Skin-->
+
+          <!--          Sound-->
+          <div class="my-5 flex justify-between">
+            <div class="flex flex-row items-start">
+              <div class="mr-6 mt-1">
+                <sound-icon />
+              </div>
+              <div class="cursor-default">
+                <p class="settings-title block">{{ $t('settingsSelectedTheme') }}</p>
+                <span v-if="theme==='dark'" class="settings-subtitle block">{{ $t('themeDark') }}</span>
+                <span v-else class="settings-subtitle block">{{ $t('themeLight') }}</span>
+              </div>
+            </div>
+            <div class="float-right align-baseline">
+              <input type="checkbox" id="sound" v-model="isDarkTheme" @change="toggleTheme" /><label for="sound">Toggle</label>
+            </div>
+          </div>
+          <!--          End sound-->
+
+        </div>
+      </div>
+<!--      End Application Section-->
+
     </div>
 
     <locale-modal v-if="selectLocal" v-on:localeCloseEvent="closeLocal"/>
+
+    <skin-modal v-if="selectSkin" v-on:skinCloseEvent="closeSkin" v-on:changeSkin="changeSkin" />
 
   </section>
 </template>
@@ -61,9 +114,15 @@ import NavBar from "./NavBar";
 import ThemeIcon from "./settings/ThemeIcon";
 import LocaleIcon from "./settings/LocaleIcon";
 import LocaleModal from "./settings/LocaleModal";
+import SkinIcon from "./settings/SkinIcon";
+import SoundIcon from "./settings/SoundIcon";
+import SkinModal from "./settings/SkinModal";
 export default {
   name: "Settings",
   components: {
+    SkinModal,
+    SoundIcon,
+    SkinIcon,
     LocaleModal,
     LocaleIcon,
     ThemeIcon,
@@ -73,16 +132,26 @@ export default {
     theme: 'light',
     isDarkTheme: false,
     selectLocal: false,
-    locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(',')
+    selectSkin: false,
+    locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(','),
+    skin: '',
   }),
   mounted() {
     this.theme = localStorage.getItem('theme');
     document.documentElement.setAttribute('data-theme', this.theme);
     this.isDarkTheme = this.theme === 'dark';
+    this.skin = localStorage.getItem('skin')??'animals';
   },
   methods: {
     closeLocal() {
       this.selectLocal = false;
+    },
+    closeSkin() {
+      this.selectSkin = false;
+    },
+    changeSkin(skinName) {
+      localStorage.setItem('skin', skinName);
+      this.skin = skinName;
     },
     onBack() {
       this.$router.back();
---
+++ src/components/settings/SkinModal.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="modal-mask" @click.self="closeModal">
+    <transition name="slide-fade">
+      <section v-if="show" class="column ml-auto mr-auto mt-4 mb-4 flex-wrap md:max-w-lg lg:max-w-xl xl:max-w-2xl transition-all">
+        <div class="card w-100 mx-2 sm:mx-4 md:m-0 shadow-md rounded-md" style="background-color: #35458D; z-index: 200">
+          <div class="modal-content">
+            <div class="flex row justify-between">
+              <div class="flex row">
+                <skin-icon class="fill-current green mr-2 mt-0" style="width: 38px"/>
+                <h2 class="green cursor-default">
+                  {{ $t('settingsExercisesSelectSkins') }}
+                </h2>
+              </div>
+              <button aria-label="Close" class="float-right" @click="closeModal" type="button">
+                <span class="white" aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <ul v-for="skin in skins" :key="skin" >
+              <li class="flex justify-between modal-tile my-5 mx-1 py-4 px-4 rounded-md cursor-pointer" @click="changeSkin(skin)">
+                <p v-if="skin===currentSkin" class="green">{{$t(skin)}}</p>
+                <p v-else class="white">{{$t(skin)}}</p>
+                <svg v-if="skin===currentSkin" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9.33331 14.5833L12.8333 18.0833L18.6666 11.0833" stroke="#0BD99B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M14 25.6667C20.4433 25.6667 25.6666 20.4433 25.6666 14C25.6666 7.55668 20.4433 2.33334 14 2.33334C7.55666 2.33334 2.33331 7.55668 2.33331 14C2.33331 20.4433 7.55666 25.6667 14 25.6667Z" stroke="#0BD99B" stroke-width="2"/>
+                </svg>
+                <svg v-else width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14 25.6667C20.4433 25.6667 25.6666 20.4433 25.6666 14C25.6666 7.55667 20.4433 2.33333 14 2.33333C7.55666 2.33333 2.33331 7.55667 2.33331 14C2.33331 20.4433 7.55666 25.6667 14 25.6667Z" stroke="#B1B3B7" stroke-width="2"/>
+                </svg>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+
+      </section>
+    </transition>
+  </div>
+</template>
+
+<script>
+import SkinIcon from "./SkinIcon";
+export default {
+  name: "SkinModal",
+  components: {SkinIcon},
+  data: () => ({
+    skins: ['animals', 'cards_suits', 'fruits', 'vegetables'],
+    currentSkin: 'animals',
+    show: false,
+  }),
+  mounted() {
+    this.show = true;
+    this.currentSkin = localStorage.getItem('skin')??'animals';
+  },
+  methods: {
+    closeModal () {
+      this.$emit('skinCloseEvent');
+    },
+    closeSlowly() {
+      this.show = false;
+      setTimeout(()=>{
+        this.closeModal();
+      }, 500);
+    },
+    changeSkin(skinName) {
+      this.currentSkin = skinName;
+      this.$emit('changeSkin', skinName);
+      this.closeSlowly();
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.slide-fade-enter-active {
+  transition: all .3s ease-in;
+}
+.slide-fade-leave-active {
+  transition: all .3s ease-out;
+}
+.slide-fade-enter, .slide-fade-leave-to
+{
+  transform: translateY(300px);
+  opacity: 0.5;
+}
+</style>
